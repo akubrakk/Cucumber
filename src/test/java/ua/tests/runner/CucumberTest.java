e8ac829d5cba380a7949ebc33837e72819500594
a.kubrak <padme@bbq.agency>
2022-11-18 00:28:33 +0200

    add reports

--- src/test/java/ua/tests/runner/CucumberTest.java
+++ src/test/java/ua/tests/runner/CucumberTest.java
@@ -8,19 +8,18 @@ import org.testng.annotations.DataProvider;
         plugin = {"pretty",
                 "html:target/cucumber-reports/CucumberTests.html",
                 "json:target/cucumber-reports/CucumberTests.json",
-                "junit:target/cucumber-reports/CucumberTests.xml"},
+                "junit:target/cucumber-reports/CucumberTests.xml",
+                "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"},
         monochrome = false,
-        tags = "@smoke",
-        glue = "ua.tests",
+//        tags = "@regression or @smoke",
+        glue = "org.example",
         features = "classpath:features"
 )
-
-
 public class CucumberTest extends AbstractTestNGCucumberTests {
+
     @Override
     @DataProvider(parallel = true)
-    public Object[][] scenarios(){
+    public Object[][] scenarios() {
         return super.scenarios();
     }
-
 }
